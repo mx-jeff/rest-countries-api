1698a2d56ddd46dc8310559b959b05b1d0191597
Jeff <mx.jeferson.12@gmail.com>
2020-03-06 23:37:41 -0300

    Updates

---
+++ precache-manifest.127e93316e1ad130d68582b7b428c6ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40dac8bbd148d7af021201587408be39",
+    "url": "/rest-countries-api/index.html"
+  },
+  {
+    "revision": "b25c9666388cb79500de",
+    "url": "/rest-countries-api/static/css/main.a96dd6c4.chunk.css"
+  },
+  {
+    "revision": "193be6e1b310650d8520",
+    "url": "/rest-countries-api/static/js/2.a8798de1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/rest-countries-api/static/js/2.a8798de1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b25c9666388cb79500de",
+    "url": "/rest-countries-api/static/js/main.39b621d8.chunk.js"
+  },
+  {
+    "revision": "7e7164559b22073bcf49",
+    "url": "/rest-countries-api/static/js/runtime-main.2f812076.js"
+  }
+]);
