3966a22549db0ac626f54b0073545ae65cba7add
Jeff <mx.jeferson.12@gmail.com>
2020-03-12 15:21:43 -0300

    add global colors

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './app.css'
 
 function App() {
   return (
-    <div className="app">
+    <div className="app light">
       <Header />
       <Router />
     </div>
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Router, Route, Switch } from 'react-router-dom'
+import createHistory from "history/createBrowserHistory";
 
 import AllFlags from './components/allFlags'
 import FlagDetails from "./components/flag";
 
 export default function router(){
     return(
-        <BrowserRouter>
+        <Router history={createHistory({ basename: process.env.PUBLIC_URL })}>
             <Switch>
                 <Route exact path='/' component={AllFlags}/>
                 <Route path='/:name' component={FlagDetails}/>
             </Switch>
-        </BrowserRouter>
+        </Router>
     )
 }
