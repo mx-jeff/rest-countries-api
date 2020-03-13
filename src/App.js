55650d8ab0d5c5181ed93c1888160541f4c9bea2
Jeff <mx.jeferson.12@gmail.com>
2020-03-13 15:45:43 -0300

    add improvised button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Router from './routes'
-import Header from './components/header'
 
 import './App.css'
+import './components/header/styles.css'
 
 function App() {
+  const [theme, setTheme] = useState(false)
+
+  const color = changeTheme(theme)
+
+  function changeTheme(theme){
+    const isDark = theme;
+    const color = isDark ? 'dark' : 'light'   
+    return color
+  }
+
   return (
-    <div className="app dark">
-      <Header />
+    <div className={`app ${color}`}>
+      <header>
+        <h1>Where in the world?</h1>
+        <button className="btn" onClick={() => setTheme(!theme)}>Change color</button>
+      </header>
+      
       <Router />
     </div>
   )
